5870c634a5b3bfc0f548f7999ab74ac55c60af6f
naveen khuntey <96486321+naveen-khuntey@users.noreply.github.com>
2024-05-21 19:20:00 +0530

    Initials

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,12 @@ import {
 } from 'react-router-dom';
 import MainLayout from './components/layouts/MainLayout';
 import Main from './components/Main';
+import Error from './components/Error';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<Main />} />
+      <Route path='*' element={<Error/>} />
     </Route>
   )
 )
---
+++ client/src/components/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div>
+      404 : The page you are searching don't exist
+    </div>
+  )
+}
+
+export default Error
